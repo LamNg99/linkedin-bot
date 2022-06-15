38bf06b1fa6bd53388545a85499f39ac465e59c8
Lam Nguyen <tunglam030699@gmail.com>
2022-06-15 16:10:44 -0400

    add connect-bot

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # Import necessary tools
 from selenium import webdriver
-import os
+from selenium.webdriver.common.by import By
 import sys
 import time
+import random
 
 # Open the Chrome driver and go to Linkedin login page
 driver = webdriver.Chrome('/Users/lamnguyen/Desktop/Projects/linkedin-bot/chromedriver')
@@ -24,20 +25,42 @@ def login_bot():
         print('Check your file existance and path!')
     
     # Login into Linkedin Profile
-    fill_usr = driver.find_element_by_xpath("//input[@name='session_key']")
-    fill_pass = driver.find_element_by_xpath("//input[@name='session_password']")
+    fill_usr = driver.find_element(By.XPATH, "//input[@name='session_key']")
+    fill_pass = driver.find_element(By.XPATH, "//input[@name='session_password']")
 
     fill_usr.send_keys(username)
     fill_pass.send_keys(password)
     time.sleep(1)
 
-    login_button = driver.find_element_by_xpath("//button[@type='submit']").click()
+    login_button = driver.find_element(By.XPATH, "//button[@type='submit']").click()
     print('Login Successfully!')
 
 login_bot()
 
 def connect_bot():
-    driver.get('https://www.linkedin.com/search/results/people/?network=%5B%22S%22%5D&origin=FACETED_SEARCH&page=1')
+    page = random.randint(1,100)
+    driver.get(f'https://www.linkedin.com/search/results/people/?network=%5B%22S%22%5D&origin=FACETED_SEARCH&page={page}')
+    time.sleep(1)
+
+    all_buttons = driver.find_elements(By.TAG_NAME, 'button')
+    connect_buttons = [btn for btn in all_buttons if btn.text == 'Connect']
+    count = 0
+
+    for btn in connect_buttons:
+        driver.execute_script('arguments[0].click()', btn)
+        time.sleep(1)
+
+        send_button = driver.find_element(By.XPATH, "//button[@aria-label='Send now']")
+        driver.execute_script('arguments[0].click()', send_button)
+        close_button = driver.find_element(By.XPATH, "//button[@aria-label='Dismiss']")
+        driver.execute_script('arguments[0].click()', close_button)
+        time.sleep(1)
+        count += 1
+    
+    print(f'You have sent {count} connect invitations')
+
+connect_bot()
+        
 
 
 
